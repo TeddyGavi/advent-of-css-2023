541183b23807b6416a6112d42946575c3de525d3
TeddyGavi <matc.davis@gmail.com>
2023-12-05 15:35:34 -0800

    basic styles on login layout

---
+++ matt/web/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,39 @@
+import { MetaTags } from '@redwoodjs/web'
+import PasswordFormField from 'src/components/PasswordFormField/PasswordFormField'
+
+const LoginPage = () => {
+  return (
+    <div className=" mx-auto h-screen w-screen bg-silverTree bg-auth-pages bg-contain bg-top bg-no-repeat ">
+      <MetaTags title="Login" description="Login page" />
+
+      <div className="flex h-full flex-col items-center justify-center p-2">
+        <div className="h-60 w-60 bg-secret-santa bg-contain bg-no-repeat lg:h-96 lg:w-96 lg:bg-center"></div>
+        <h1 className="relative text-center  font-condensed text-7xl uppercase tracking-wide text-white">
+          <span className="absolute -left-1/2 top-[40%]  w-1/3 border-b-8 border-b-white"></span>
+          Login
+          <span className="absolute -right-1/2 top-[40%]  w-1/3 border-b-8 border-b-white"></span>
+        </h1>
+        <form className="relative">
+          <input
+            id="username"
+            type="text"
+            required
+            placeholder=""
+            className="mb-4"
+          />
+          <label htmlFor="username">username</label>
+
+          <PasswordFormField />
+          <button className="my-4 h-16 w-full rounded-full bg-supernova text-center font-handwriting text-xl uppercase">
+            Submit
+          </button>
+          <p className="text-center underline underline-offset-4 ">
+            Need an Account?
+          </p>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
