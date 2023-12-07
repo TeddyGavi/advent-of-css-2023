fb0abb6e919bfa5af5f275bb7bc42acc20a18598
TeddyGavi <matc.davis@gmail.com>
2023-12-06 22:36:37 -0800

    basic layout

--- matt/web/src/pages/LoginPage/LoginPage.tsx
+++ matt/web/src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,20 @@
 import { MetaTags } from '@redwoodjs/web'
 import PasswordFormField from 'src/components/PasswordFormField/PasswordFormField'
+import AuthLayout from 'src/layouts/AuthLayout/AuthLayout'
+import FooterLayout from 'src/layouts/FooterLayout/FooterLayout'
 
+{
+  /* <div className=" mx-auto h-screen w-screen bg-silverTree bg-auth-pages bg-contain bg-top bg-no-repeat "> */
+}
+{
+  /* <div className="h-60 w-60 bg-secret-santa bg-contain bg-no-repeat lg:h-96 lg:w-96 lg:bg-center"></div> */
+}
 const LoginPage = () => {
   return (
-    <div className=" mx-auto h-screen w-screen bg-silverTree bg-auth-pages bg-contain bg-top bg-no-repeat ">
+    <AuthLayout>
       <MetaTags title="Login" description="Login page" />
 
       <div className="flex h-full flex-col items-center justify-center p-2">
-        <div className="h-60 w-60 bg-secret-santa bg-contain bg-no-repeat lg:h-96 lg:w-96 lg:bg-center"></div>
         <h1 className="relative text-center  font-condensed text-7xl uppercase tracking-wide text-white">
           <span className="absolute -left-1/2 top-[40%]  w-1/3 border-b-8 border-b-white"></span>
           Login
@@ -32,7 +39,8 @@ const LoginPage = () => {
           </p>
         </form>
       </div>
-    </div>
+      <FooterLayout></FooterLayout>
+    </AuthLayout>
   )
 }
 
