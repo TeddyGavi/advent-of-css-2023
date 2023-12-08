ee47bfe0ca9c589cbefd04b4cf39ec8e04aaed6d
TeddyGavi <matc.davis@gmail.com>
2023-12-07 19:57:06 -0800

    ham doesn't animate quite right...

--- matt/web/src/components/HamMenu/HamMenu.tsx
+++ matt/web/src/components/HamMenu/HamMenu.tsx
@@ -10,25 +10,29 @@ const HamMenu = () => {
   return (
     <button
       onClick={handleClick}
-      className="flex flex-col items-center justify-center gap-3 bg-supernova px-[10px] py-[15px]"
+      className="relative h-[58px] w-[69px] bg-supernova "
     >
-      <span
-        className={`block h-[2px] w-[49px] bg-black transition-all duration-300 ease-out ${
-          click ? 'origin-top-left translate-y-1/2 rotate-45 transform' : ''
+      <div
+        className={`absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 transform   px-[10px] py-[15px] ${
+          click ? ' flex' : 'flex  flex-col items-center justify-between'
         }`}
-      ></span>
-      <span
-        className={`h-[2px] w-[49px] bg-black transition-all duration-300 ease-out ${
-          click ? 'opacity-0' : 'opacity-100'
-        }`}
-      ></span>
-      <span
-        className={`h-[2px] w-[49px] bg-black transition-all duration-300 ease-out ${
-          click
-            ? 'origin-bottom-left -translate-y-1/2  -rotate-45 transform'
-            : ''
-        }`}
-      ></span>
+      >
+        <span
+          className={` h-[2px] w-[49px] bg-black transition-all duration-300 ease-out ${
+            click ? 'absolute top-1/2 rotate-45  ' : 'block '
+          }`}
+        ></span>
+        <span
+          className={`h-[2px]  w-[49px] bg-black transition-all duration-300 ease-out ${
+            click ? 'hidden opacity-0' : 'block opacity-100'
+          }`}
+        ></span>
+        <span
+          className={`h-[2px] w-[49px]  bg-black transition-all duration-300 ease-out ${
+            click ? 'absolute top-1/2 -rotate-45' : ' block'
+          }`}
+        ></span>
+      </div>
     </button>
   )
 }
