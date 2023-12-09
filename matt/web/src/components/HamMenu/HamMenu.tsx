c60464f4d836e6779c7439e33bd26255319681ea
teddygavi <matc.davis@gmail.com>
2023-12-09 01:20:34 -0600

    using amy's solution

--- matt/web/src/components/HamMenu/HamMenu.tsx
+++ matt/web/src/components/HamMenu/HamMenu.tsx
@@ -1,38 +1,28 @@
 import { useState } from 'react'
 
 const HamMenu = () => {
-  const [click, setClicked] = useState(false)
+  const [isClicked, setIsClicked] = useState(true)
 
-  const handleClick = () => {
-    setClicked((prev) => !prev)
+  const toggleNav = () => {
+    setIsClicked(prev => !prev)
   }
 
   return (
     <button
-      onClick={handleClick}
-      className="relative h-[58px] w-[69px] bg-supernova "
+      className="flex h-[58px] w-[68px] flex-col items-center justify-center bg-supernova"
+      onClick={toggleNav}
     >
       <div
-        className={`absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 transform   px-[10px] py-[15px] ${
-          click ? ' flex' : 'flex  flex-col items-center justify-between'
+        className={`absolute h-[3px] w-12  bg-black transition-transform ${
+          isClicked ? '-translate-y-3' : 'rotate-45'
         }`}
-      >
-        <span
-          className={` h-[2px] w-[49px] bg-black transition-all duration-300 ease-out ${
-            click ? 'absolute top-1/2 rotate-45  ' : 'block '
-          }`}
-        ></span>
-        <span
-          className={`h-[2px]  w-[49px] bg-black transition-all duration-300 ease-out ${
-            click ? 'hidden opacity-0' : 'block opacity-100'
-          }`}
-        ></span>
-        <span
-          className={`h-[2px] w-[49px]  bg-black transition-all duration-300 ease-out ${
-            click ? 'absolute top-1/2 -rotate-45' : ' block'
-          }`}
-        ></span>
-      </div>
+      />
+      <div className={`h-[3px] w-12 bg-black ${isClicked ? '' : 'hidden'}`} />
+      <div
+        className={`absolute h-[3px] w-12 bg-black transition-transform ${
+          isClicked ? 'translate-y-3' : '-rotate-45'
+        }`}
+      />
     </button>
   )
 }
