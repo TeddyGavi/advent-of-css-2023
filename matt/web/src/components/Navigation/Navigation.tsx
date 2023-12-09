697cc714b179009e2d4c21896a18408f5496019e
TeddyGavi <matc.davis@gmail.com>
2023-12-09 15:16:21 -0800

    init creation

---
+++ matt/web/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+  return (
+    <div>
+      <h2>{'Navigation'}</h2>
+      <p>{'Find me in ./web/src/components/Navigation/Navigation.tsx'}</p>
+    </div>
+  )
+}
+
+export default Navigation
