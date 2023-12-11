6983c7b7a33fa5681df40d1ae505b1adb6691110
TeddyGavi <matc.davis@gmail.com>
2023-12-10 21:53:42 -0800

    final, will check solution to make sure I'm on track!
    and changed bg

--- matt/web/src/components/AccountDropDown/AccountDropDown.tsx
+++ matt/web/src/components/AccountDropDown/AccountDropDown.tsx
@@ -1,32 +1,54 @@
+import { useState } from 'react'
 import AvatarImgOnly from '../AvatarImgOnly/AvatarImgOnly'
 import Icon, { colors } from '../icons/Icon'
 
 const AccountDropDown = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false)
+
+  const toggleDropDown = () => {
+    setIsDropDownOpen((prev) => !prev)
+  }
+
   return (
     <>
-      <div className="flex w-60 items-center justify-around gap-3 font-sans">
-        <Icon id="chevron" color={colors.black} />
+      <div className="z-10 flex w-60 items-center justify-around gap-3 font-sans ">
+        <button
+          onClick={toggleDropDown}
+          className={`transition-transform duration-300 ${
+            isDropDownOpen ? '' : '-rotate-90 '
+          }`}
+        >
+          <Icon id="chevron" color={colors.black} />
+        </button>
         <AvatarImgOnly image="avatar-05.png" />
         <div className="flex flex-col items-center justify-center ">
           <div>Logged in as</div>
           <div className="text-lg font-bold">Cody Fisher</div>
         </div>
       </div>
-      <div className="absolute  left-12 top-28 h-40 w-48 border bg-supernova">
-        <div className="absolute  -top-3 left-8 -z-10 h-6 w-6 rotate-45 border border-red-900"></div>
-        <div className="my-auto flex h-full flex-col items-start justify-between px-4 py-6 font-condensed text-2xl uppercase">
-          <div className="dropdown-list">
-            <Icon id="user" color={colors.pastelMagenta} size={20} />
-            My Account
-          </div>
-          <div className="dropdown-list">
-            <Icon id="calendar" color={colors.pastelMagenta} size={20} />
-            My Event
-          </div>
-          <div className="dropdown-list">
-            <Icon id="logout" color={colors.pastelMagenta} size={20} />
-            Logout
-          </div>
+      <div
+        className={`absolute left-12 top-28 -z-10 h-40 w-48 translate-y-0 transform transition-all duration-500 ${
+          isDropDownOpen
+            ? 'translate-y-0 opacity-100'
+            : ' -translate-y-1/3 opacity-0'
+        }`}
+      >
+        <div className="absolute  -top-3 left-8 -z-20 h-6 w-6 rotate-45"></div>
+        <div className="my-auto flex h-full flex-col items-start justify-between px-4 py-6 font-condensed text-3xl uppercase">
+          <ul className="dropdown-list">
+            <li>
+              <Icon id="user" color={colors.pastelMagenta} size={30} />
+              <a href="#">My Account</a>
+            </li>
+            <li>
+              <Icon id="calendar" color={colors.pastelMagenta} size={30} />
+              <a href="#">My Event</a>
+            </li>
+            <li>
+              <Icon id="logout" color={colors.pastelMagenta} size={30} />
+              <a href="#">Logout</a>
+            </li>
+          </ul>
         </div>
       </div>
     </>
