94f2bcf7e289c6cb9e66c9a7887bc3b6d351b98c
TeddyGavi <matc.davis@gmail.com>
2023-12-12 23:37:04 -0800

    edits to password and basic styles on rscp accept page

--- matt/web/src/pages/RSVPAcceptPage/RSVPAcceptPage.tsx
+++ matt/web/src/pages/RSVPAcceptPage/RSVPAcceptPage.tsx
@@ -1,20 +1,36 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import PasswordFormField from 'src/components/PasswordFormField/PasswordFormField'
+import Icon, { colors } from 'src/components/icons/Icon'
+import AuthLayout from 'src/layouts/AuthLayout/AuthLayout'
 
 const RsvpAcceptPage = () => {
   return (
     <>
       <MetaTags title="RsvpAccept" description="RsvpAccept page" />
-
-      <h1>RsvpAcceptPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/RSVPAcceptPage/RSVPAcceptPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>rsvpAccept</code>, link to me with `
-        <Link to={routes.rsvpAccept()}>RsvpAccept</Link>`
-      </p>
+      <AuthLayout>
+        <div className="flex h-full flex-col items-center justify-center p-2 text-center font-condensed uppercase tracking-wide text-white">
+          <h1 className="relative text-7xl">
+            <span className="absolute -left-[30%] top-[40%]  w-1/4 border-b-8 border-b-white"></span>
+            Awesome
+            <span className="absolute -right-[30%] top-[40%]  w-1/4 border-b-8 border-b-white"></span>
+          </h1>
+          <h2 className="text-8xl">Sign Up</h2>
+        </div>
+        <div className=" mx-auto flex w-10/12 flex-col items-center justify-center gap-6">
+          <PasswordFormField></PasswordFormField>
+          <div className="flex h-28 w-full border-spacing-0.5 items-center justify-between border-4 border-dashed px-6 text-white">
+            <div className="font-handwriting text-xl uppercase">Avatar</div>
+            <div className="font-sans text-padua opacity-90">
+              Drag and Drop an Image
+            </div>
+            <Icon id="upload" color={colors.white} />
+          </div>
+          <button className="my-4 h-16 w-full rounded-full bg-supernova text-center font-handwriting text-xl uppercase">
+            Submit
+          </button>
+        </div>
+      </AuthLayout>
     </>
   )
 }
