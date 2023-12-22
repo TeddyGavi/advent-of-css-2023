918d00a04f6cf947b1698b1a6207ebb7abc6b52f
TeddyGavi <matc.davis@gmail.com>
2023-12-21 23:15:47 -0800

    init wish list panel

---
+++ matt/web/src/components/IndividualWishListSlideOut/IndividualWishListSlideOut.tsx
@@ -0,0 +1,11 @@
+import InteriorLayout from 'src/layouts/InteriorLayout/InteriorLayout'
+
+const IndividualWishListSlideOut = () => {
+  return (
+    <aside className="absolute right-0 h-screen bg-spanishGreen p-10 text-white">
+      <h1 className="font-sans text-7xl  uppercase">Wish List</h1>
+    </aside>
+  )
+}
+
+export default IndividualWishListSlideOut
