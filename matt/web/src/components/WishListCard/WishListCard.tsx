b7c129254eeb5eaddf1f3df478499a21e7319496
TeddyGavi <matc.davis@gmail.com>
2024-01-04 22:36:31 -0800

    working on layout

--- matt/web/src/components/WishListCard/WishListCard.tsx
+++ matt/web/src/components/WishListCard/WishListCard.tsx
@@ -1,24 +1,33 @@
 const WishListCard = () => {
   return (
-    <div>
-      <div className="flex justify-between">
-        <div>1 iPhone 15</div>
-        <div>details button</div>
-      </div>
-      <div className="flex justify-evenly border border-padua ">
-        <div className="p-4">
-          <div>Url to purchase</div>
-          <div>title</div>
-          <p>
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-            cillum sint consectetur cupidatat.
-          </p>
+    <div className="flex bg-spanishGreen text-white">
+      <span className="mr-4  h-full  font-handwriting text-3xl">1</span>
+      <div>
+        <div className="flex items-center justify-between font-sans text-lg font-bold ">
+          <div className="py-5 text-3xl">iPhone 15</div>
+          <button className="h-8 w-28 rounded-full bg-cruseo text-center  text-sm font-bold uppercase ">
+            Details
+          </button>
         </div>
-        <img
-          src="images/image-iphone.png"
-          alt="detail photo of wish list item"
-        />
+        <div className=" flex justify-evenly border border-padua ">
+          <div className="flex flex-col justify-evenly gap-4 p-4">
+            <div>APPLE.COM</div>
+            <div className="text-2xl font-bold">
+              iPhone 15 and iPhone 15 Plus
+            </div>
+            <p>
+              iPhone 15 and iPhone 15 Plus. Dynamic Island. 48MP Main camera
+              with 2x Telephoto. All-day battery life. USB-C. 6.1” and 6.7”
+              sizes.
+            </p>
+          </div>
+          <img
+            src="images/image-iphone.png"
+            alt="detail photo of wish list item"
+          />
+        </div>
       </div>
+      <hr className="my-8 " />
     </div>
   )
 }
